3ec7092fcfeb498cf91055513098b14e93725d0e
sbloch3 <sbloch3@wisc.edu>
2021-11-21 19:27:09 -0600

    Create debugVariableDrillsV2.py

---
+++ debugVariableDrillsV2.py
@@ -0,0 +1,67 @@
+
+'''
+The following functions have problems that keep them from
+completing the task that they have to do.
+
+All the problems are either Logical or Syntactical errors with VARIABLES.
+Focus on the variables and find the problems with the VARIABLES.
+
+The number of errors are as follows:
+volumeCalculator: 4 errors
+shippingAndTax: 3 errors
+circleArea: 3 errors
+'''
+
+'''
+The goal of this function is to calculate the volume of
+an object, when the user inputs height, width, and depth.
+
+The function should print the sentence plus the volume and
+return the volume.
+'''
+def volumeCalculator(height, width, depth):
+    volume = volumeCalculator[2] * area
+    area = volumeCalculator[0] * volumeCalculator[1]
+    sentence = "The volume of this object is: "
+    print(sentence + str(volumn))
+    return volumn
+
+#Leave the next line alone
+volumeCalculator(5, 5, 5)
+
+'''
+The goal of this function is to calculate the 
+total of an order after tax and shipping.
+
+The function should print and
+return the total
+'''
+def shippingAndTax(subTotal):
+    shipping = 10
+    tax = .10
+    
+    taxTotal = shippingAndTax(subTotal) * tax
+    total = subTotal + taxTotal + shipping
+    print("The total is: " + str(total))
+    return total
+
+#Leave the next line alone
+shippingAndTax(15)
+
+'''
+The goal of this function is to calculate the 
+total of an order after tax and shipping.
+
+The function should print and
+return the total
+'''
+def circleArea(area):
+    pi = 3.14
+    
+    area = pi * squared 
+    squared = radius * radius
+    print("The area is: " + str(area))
+    return area
+
+#Leave the next line alone
+circleArea(5)
